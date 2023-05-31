07d5f636f7162e61f483bd3c81e3d6f067ebba2d
Kyuwoong Lee <kwooler@gmail.com>
2023-05-30 20:46:01 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    s = input()
+    indicies = list(map(int, input().split()))
+    ##################################################
+    # Code your program here
+    ##################################################
+    # You should use the variable name "result" to save the result string
+
+    ##################################################
+    # DO NOT Delete the return statement
+    print(result)
+    return result
+##
+
+
+if __name__ == '__main__':
+    main()
